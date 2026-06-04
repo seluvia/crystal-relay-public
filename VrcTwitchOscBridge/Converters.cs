ff4e1e5bcac2961e019034ede13ce1c3bd40ac79
Screminpal <seluvia1@gmail.com>
2026-06-03 20:13:37 -0500

    feat: add EnumBooleanConverter for avatar picker view mode toggle

---
+++ VrcTwitchOscBridge/Converters.cs
@@ -0,0 +1,52 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace VrcTwitchOscBridge;
+
+public sealed class EnumBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null || parameter is null)
+        {
+            return false;
+        }
+
+        var enumValue = value.ToString();
+        var targetValue = parameter.ToString();
+        return string.Equals(enumValue, targetValue, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is true && parameter is not null)
+        {
+            return Enum.Parse(targetType, parameter.ToString()!);
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+}
+
+public sealed class EnumToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null || parameter is null)
+        {
+            return Visibility.Collapsed;
+        }
+
+        var enumValue = value.ToString();
+        var targetValue = parameter.ToString();
+        return string.Equals(enumValue, targetValue, StringComparison.OrdinalIgnoreCase)
+            ? Visibility.Visible
+            : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
