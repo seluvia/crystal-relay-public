88e20f0c4e244b99cf705c2e02a1841e0de0020c
Screminpal <seluvia1@gmail.com>
2026-06-04 02:19:44 -0500

    feat: add Wardrobe editor UI to MainWindow.xaml

--- VrcTwitchOscBridge/Converters.cs
+++ VrcTwitchOscBridge/Converters.cs
@@ -50,3 +50,22 @@ public sealed class EnumToVisibilityConverter : IValueConverter
         throw new NotSupportedException();
     }
 }
+
+[ValueConversion(typeof(bool), typeof(bool))]
+public sealed class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is bool b && !b;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is bool b && !b;
+}
+
+public sealed class NullToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is null ? Visibility.Collapsed : Visibility.Visible;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
