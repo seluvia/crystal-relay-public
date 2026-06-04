58a18d91c86a246519d4c676cbb2385565a171fd
Screminpal <seluvia1@gmail.com>
2026-06-03 19:26:53 -0500

    feat: add AvatarLibraryManagerWindow with Groups and Tags tabs

---
+++ VrcTwitchOscBridge/AvatarLibraryManagerWindow.xaml.cs
@@ -0,0 +1,50 @@
+using System.Windows;
+using System.Windows.Input;
+using VrcTwitchOscBridge.Models;
+using VrcTwitchOscBridge.Services;
+using VrcTwitchOscBridge.ViewModels;
+
+namespace VrcTwitchOscBridge;
+
+public partial class AvatarLibraryManagerWindow : Window
+{
+    private readonly AvatarLibraryManagerViewModel viewModel;
+
+    public AvatarLibraryManagerWindow(
+        AppTheme theme,
+        AvatarLibrary library,
+        AvatarImageService imageService)
+    {
+        viewModel = new AvatarLibraryManagerViewModel(library, imageService);
+        DataContext = viewModel;
+
+        InitializeComponent();
+        ThemeManager.ApplyToResources(Resources, theme);
+        ThemeManager.ThemeChanged += OnThemeManagerThemeChanged;
+        Closed += OnWindowClosed;
+    }
+
+    private void OnThemeManagerThemeChanged(object? sender, EventArgs e)
+    {
+        Dispatcher.BeginInvoke(() => ThemeManager.ApplyToResources(Resources));
+    }
+
+    private void OnWindowClosed(object? sender, EventArgs e)
+    {
+        ThemeManager.ThemeChanged -= OnThemeManagerThemeChanged;
+        Closed -= OnWindowClosed;
+    }
+
+    private void OnCloseButtonClicked(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            try { DragMove(); } catch { }
+        }
+    }
+}
