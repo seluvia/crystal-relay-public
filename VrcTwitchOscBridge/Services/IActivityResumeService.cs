d2bf4ce5b4e4b009911361b22c3de7e3766f5ec3
Screminpal <seluvia1@gmail.com>
2026-06-07 21:21:09 -0500

    feat: add IActivityResumeService interface

---
+++ VrcTwitchOscBridge/Services/IActivityResumeService.cs
@@ -0,0 +1,15 @@
+using VrcTwitchOscBridge.Models;
+
+namespace VrcTwitchOscBridge.Services;
+
+public interface IActivityResumeService
+{
+    Task LoadPendingAsync();
+    bool HasPendingResume { get; }
+    bool IsPendingForAvatar(string avatarId);
+    IReadOnlyList<ResumeActivity> GetPendingActivities();
+    Task RecordActivityStartedAsync(ResumeActivity activity);
+    Task RecordActivityEndedAsync(Guid ruleId);
+    Task ClearAllAsync();
+    Task CommitAsync();
+}
