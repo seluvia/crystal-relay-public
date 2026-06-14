68123665424aca395af7f9a647c5036f2031bbc6
Screminpal <seluvia1@gmail.com>
2026-06-14 15:40:00 -0500

    feat(universal-triggers): add UniversalTriggerFloatConverter

--- VrcTwitchOscBridge/Converters/UniversalTriggerValueConverters.cs
+++ VrcTwitchOscBridge/Converters/UniversalTriggerValueConverters.cs
@@ -88,3 +88,54 @@ public sealed class UniversalTriggerIntConverter : IValueConverter
             : 0;
     }
 }
+
+[ValueConversion(typeof(string), typeof(string))]
+public sealed class UniversalTriggerFloatConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+        {
+            return "0";
+        }
+
+        var text = value.ToString();
+        if (string.IsNullOrEmpty(text))
+        {
+            return "0";
+        }
+
+        if (double.TryParse(text, NumberStyles.Float, CultureInfo.InvariantCulture, out var doubleValue))
+        {
+            if (doubleValue == Math.Truncate(doubleValue)
+                && !double.IsInfinity(doubleValue)
+                && doubleValue >= -1e15
+                && doubleValue <= 1e15)
+            {
+                return ((long)doubleValue).ToString(CultureInfo.InvariantCulture);
+            }
+
+            return doubleValue.ToString("R", CultureInfo.InvariantCulture);
+        }
+
+        return text;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+        {
+            return 0.0;
+        }
+
+        var text = value.ToString();
+        if (string.IsNullOrEmpty(text))
+        {
+            return 0.0;
+        }
+
+        return double.TryParse(text, NumberStyles.Float, CultureInfo.InvariantCulture, out var parsed)
+            ? parsed
+            : 0.0;
+    }
+}
