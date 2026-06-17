fa3d2a255b23e54a917cdcd48b2122f2f31d0af8
Screminpal <seluvia1@gmail.com>
2026-06-16 23:46:56 -0500

    feat(avatar-swap): implement InlineAvatarSwapRuleRowControl XAML (stub inline editor)

--- VrcTwitchOscBridge/UserControls/InlineAvatarSwapRuleRowControl.xaml.cs
+++ VrcTwitchOscBridge/UserControls/InlineAvatarSwapRuleRowControl.xaml.cs
@@ -1,8 +1,20 @@
+using System.Windows;
 using System.Windows.Controls;
+using VrcTwitchOscBridge.ViewModels;
 
 namespace VrcTwitchOscBridge.UserControls;
 
 public partial class InlineAvatarSwapRuleRowControl : UserControl
 {
+    public static readonly DependencyProperty RowProperty = DependencyProperty.Register(
+        nameof(Row), typeof(InlineAvatarSwapRuleRowViewModel), typeof(InlineAvatarSwapRuleRowControl),
+        new PropertyMetadata(null));
+
+    public InlineAvatarSwapRuleRowViewModel? Row
+    {
+        get => (InlineAvatarSwapRuleRowViewModel?)GetValue(RowProperty);
+        set => SetValue(RowProperty, value);
+    }
+
     public InlineAvatarSwapRuleRowControl() => InitializeComponent();
 }
