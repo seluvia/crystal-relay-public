0ebc62cf09544374d3180e9e4f3a9f9f187179a8
Screminpal <seluvia1@gmail.com>
2026-06-16 23:51:22 -0500

    feat(avatar-swap): rewrite AvatarSwapManagerWindow with 4-section right panel + roulette cards

--- VrcTwitchOscBridge/AvatarSwapManagerWindow.xaml.cs
+++ VrcTwitchOscBridge/AvatarSwapManagerWindow.xaml.cs
@@ -1,6 +1,4 @@
 using System.Windows;
-using System.Windows.Input;
-using VrcTwitchOscBridge.Services;
 using VrcTwitchOscBridge.ViewModels;
 
 namespace VrcTwitchOscBridge;
@@ -14,55 +12,7 @@ public partial class AvatarSwapManagerWindow : Window
         InitializeComponent();
         _viewModel = viewModel ?? throw new System.ArgumentNullException(nameof(viewModel));
         DataContext = _viewModel;
-        ThemeManager.ThemeChanged += OnThemeChanged;
-        ThemeManager.ApplyToResources(Resources);
-        Closed += OnClosed;
     }
 
     public AvatarSwapManagerViewModel ViewModel => _viewModel;
-
-    private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
-    {
-        if (e.ChangedButton == MouseButton.Left)
-        {
-            try
-            {
-                DragMove();
-            }
-            catch
-            {
-            }
-        }
-    }
-
-    private void OnCloseClicked(object sender, RoutedEventArgs e)
-    {
-        Close();
-    }
-
-    private void OnChannelPointSectionHeaderClick(object sender, MouseButtonEventArgs e)
-    {
-        _viewModel.ToggleChannelPointSectionCommand.Execute(null);
-    }
-
-    private void OnBitsSubsSectionHeaderClick(object sender, MouseButtonEventArgs e)
-    {
-        _viewModel.ToggleBitsSubsSectionCommand.Execute(null);
-    }
-
-    private void OnRouletteSectionHeaderClick(object sender, MouseButtonEventArgs e)
-    {
-        _viewModel.ToggleRouletteSectionCommand.Execute(null);
-    }
-
-    private void OnThemeChanged(object? sender, System.EventArgs e)
-    {
-        Dispatcher.InvokeAsync(() => ThemeManager.ApplyToResources(Resources));
-    }
-
-    private void OnClosed(object? sender, System.EventArgs e)
-    {
-        ThemeManager.ThemeChanged -= OnThemeChanged;
-        _viewModel.OnWindowClosed();
-    }
 }
