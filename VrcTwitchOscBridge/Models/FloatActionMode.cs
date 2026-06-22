e70e931a4857dd360d4b3a437378b5769a999243
Screminpal <seluvia1@gmail.com>
2026-06-21 20:43:19 -0500

    feat(float-modes): add FloatActionMode enum

---
+++ VrcTwitchOscBridge/Models/FloatActionMode.cs
@@ -0,0 +1,15 @@
+namespace VrcTwitchOscBridge.Models;
+
+public enum FloatActionMode
+{
+    Set = 0,
+    Random = 1,
+    Add = 2,
+    Subtract = 3,
+    AddSubtract = 4,
+    Multiply = 5,
+    Toggle = 6,
+    Cycle = 7,
+    Glitchy = 8,
+    Pulse = 9,
+}
